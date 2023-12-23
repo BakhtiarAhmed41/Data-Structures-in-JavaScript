dea67dab4bd091ab4fc850d6c6b1cbddf50705b9
BakhtiarAhmed41 <ahmedbakhtiar41@gmail.com>
2023-12-23 23:26:43 +0500

    Traversing an array

---
+++ traversing_array.js
@@ -0,0 +1,13 @@
+//Given an array arr, print all its elements
+
+function traverseArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+function main() {
+    let arr = [3, 5, 45, 32, 90];
+
+    traverseArray(arr);
+}
