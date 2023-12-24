c831c89661a47fb3f11367bce0e2d2010ec3197a
BakhtiarAhmed41 <ahmedbakhtiar41@gmail.com>
2023-12-24 23:53:42 +0500

    added some comments

--- traversing_array.js
+++ traversing_array.js
@@ -2,12 +2,12 @@
 
 function traverseArray(arr) {
     for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
+        console.log(arr[i]);   //Printing each element of array one by one
     }
 }
 
 function main() {
     let arr = [3, 5, 45, 32, 90];
 
-    traverseArray(arr);
+    traverseArray(arr); //Calling the function with an array passed in argument
 }
