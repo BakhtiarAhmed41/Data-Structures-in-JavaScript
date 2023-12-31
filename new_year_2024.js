604c0d878fe4c6d025f6230541254a2e7d44221e
BakhtiarAhmed41 <ahmedbakhtiar41@gmail.com>
2023-12-31 23:23:51 +0500

    Good bye 2023!  Welcome 2024!

--- new_year_2024.js
+++ new_year_2024.js
@@ -1,21 +1,18 @@
-// Goodbye 2023 messages
 const goodbyeMessages = [
     "Farewell 2023!",
     "Goodbye, and thanks for the memories, 2023!",
     "Adios 2023, it's been a blast!"
 ];
 
-// Welcome 2024 messages
 const welcomeMessages = [
     "Hello 2024! Let's make it amazing!",
     "Welcome 2024, may it bring prosperity!",
     "Here's to a fantastic 2024 ahead!"
 ];
 
-// Randomly select messages from each array
 const randomGoodbye = goodbyeMessages[Math.floor(Math.random() * goodbyeMessages.length)];
 const randomWelcome = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
 
-// Display the messages
+
 console.log(randomGoodbye);
 console.log(randomWelcome);
