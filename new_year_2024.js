4ea37b6e025b8eafcf422c2a693d712a2ec7ec6e
BakhtiarAhmed41 <ahmedbakhtiar41@gmail.com>
2024-01-03 23:43:18 +0500

    some changes

--- new_year_2024.js
+++ new_year_2024.js
@@ -1,17 +1,10 @@
 const goodbyeMessages = [
-    "Farewell 2023!",
     "Goodbye, and thanks for the memories, 2023!",
-    "Adios 2023, it's been a blast!"
 ];
 
 const welcomeMessages = [
     "Hello 2024! Let's make it amazing!",
-    "Welcome 2024, may it bring prosperity!",
-    "Here's to a fantastic 2024 ahead!"
 ];
-
-const randomGoodbye = goodbyeMessages[Math.floor(Math.random() * goodbyeMessages.length)];
-const randomWelcome = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
 
 
 console.log(randomGoodbye);
