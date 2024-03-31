6183759314822ddde06f4e43d45357ed9eabc721
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2024-03-31 08:26:18 +0500

    Create merging_2_sorted_linked-lists.js

---
+++ merging_2_sorted_linked-lists.js
@@ -0,0 +1,135 @@
+// Given pointers to the heads of two sorted linked lists, merge them into a single, sorted linked list.
+// Either head pointer may be null meaning that the corresponding list is empty.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function mergeLists(head1, head2) {
+    let mergedHead = new SinglyLinkedListNode();
+    let temp = mergedHead;
+    
+    while (head1 !== null && head2 !== null) {
+        if (head1.data < head2.data) {
+            temp.next = new SinglyLinkedListNode(head1.data);
+            head1 = head1.next;
+        } else {
+            temp.next = new SinglyLinkedListNode(head2.data);
+            head2 = head2.next;
+        }
+        temp = temp.next;
+    }
+    
+    if (head1 !== null) {
+        temp.next = head1;
+    } else {
+        temp.next = head2;
+    }
+
+    return mergedHead.next;
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const llist1Count = parseInt(readLine(), 10);
+
+        let llist1 = new SinglyLinkedList();
+
+        for (let i = 0; i < llist1Count; i++) {
+            const llist1Item = parseInt(readLine(), 10);
+            llist1.insertNode(llist1Item);
+        }
+      
+      	const llist2Count = parseInt(readLine(), 10);
+
+        let llist2 = new SinglyLinkedList();
+
+        for (let i = 0; i < llist2Count; i++) {
+            const llist2Item = parseInt(readLine(), 10);
+            llist2.insertNode(llist2Item);
+        }
+
+        let llist3 = mergeLists(llist1.head, llist2.head);
+
+        printSinglyLinkedList(llist3, " ", ws)
+        ws.write("\n");
+    }
+
+    ws.end();
+}
